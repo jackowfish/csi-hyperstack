65e1828a28a98c25cd8f63619f89104b71c1befa
Jack Decker <jack@thundercompute.com>
2025-10-02 16:35:34 +0000

    Merge branch 'jack/update-volume-unmount-verification' into jack/update-max-volumes

--- pkg/driver/controllerserver.go
+++ pkg/driver/controllerserver.go
@@ -336,7 +336,7 @@ func (cs *controllerServer) ControllerUnpublishVolume(ctx context.Context, req *
 		}
 		return nil, status.Error(codes.DeadlineExceeded, "ControllerUnpublishVolume: Timeout waiting for volume to detach")
 	}
-	return &csi.ControllerUnpublishVolumeResponse{}, nil
+	return nil, status.Errorf(codes.Internal, "ControllerPublishVolume: Volume in unexpected state: %s", *getVolume.Status)
 }
 
 func (cs *controllerServer) ListVolumes(ctx context.Context, req *csi.ListVolumesRequest) (*csi.ListVolumesResponse, error) {
